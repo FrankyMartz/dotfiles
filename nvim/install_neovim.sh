57f835c20c535ca85ff006badc3697e8054bc353
Franky Martinez <FrankyMartz@gmail.com>
2015-06-16 21:46:50 -0500

    [UPDATE]Replace (Vundle) -> (Plug-Vim)
    Performance, RAM usage.

--- nvim/install_neovim.sh
+++ nvim/install_neovim.sh
@@ -20,43 +20,23 @@ __configNVIM(){
     #===========================================================================
     # Symbolic Links
     #===========================================================================
-    [[ -L ~/.nvim ]] || ln -fs $NEOVIM ~/.nvim
-    [[ -L ~/.nvimrc ]] || ln -fs ${NEOVIM}/nvimrc ~/.nvimrc
+    [[ -L ~/.nvim ]] || ln -fs "$NEOVIM" ~/.nvim
+    [[ -L ~/.nvimrc ]] || ln -fs "${NEOVIM}/nvimrc" ~/.nvimrc
 
     #===========================================================================
     # Neovim Plugins
     #===========================================================================
     local BUNDLE="${NEOVIM}/bundle"
     if [[ ! -d "${BUNDLE}" ]]; then
-        # Vundle ---------------------------------------------------------------
-        dLog "${GREEN}==> Installing Neovim Vundle..."
-        mkdir $BUNDLE
-        /usr/bin/env git clone https://github.com/gmarik/Vundle.vim.git $BUNDLE/vundle
-        dLog "${GREEN}==> Installing Neovim Vundle...DONE"
-
         # Plugins --------------------------------------------------------------
         dLog "${GREEN}==> Installing Neovim Plugins..."
         /usr/bin/env pip install neovim
-        /usr/bin/env nvim +PluginInstall +qall
         dLog "${GREEN}==> Installing Neovim Plugins...DONE"
-
-        # TernJS ---------------------------------------------------------------
-        dLog "${GREEN}==> Installing TernJS..."
-        cd ${BUNDLE}/tern_for_vim
-        /usr/bin/env npm install
-        cd $PWD
-        dLog "${GREEN}==> Installing TernJS...DONE"
-
-        # YCM ------------------------------------------------------------------
-        dLog "${GREEN}==> Compiling YouCompleteMe..."
-        cd $BUNDLE/YouCompleteMe
-        source install.sh
-        cd $PWD
-        dLog "${GREEN}==> Compiling YouCompleteMe...DONE"
 
     else
         dLog "${GREEN}==> Updating Neovim Plugins..."
-        /usr/bin/env nvim +PluginUpdate +qall
+        /usr/bin/env pip install -U neovim
+        /usr/bin/env nvim +PlugUpdate +qall
         dLog "${GREEN}==> Updating Neovim Plugins...DONE"
     fi
 
