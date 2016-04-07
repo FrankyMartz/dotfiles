2790eeafe49b9558f915af60324c08a7127d9e2d
Franky Martinez <FrankyMartz@gmail.com>
2016-04-06 20:40:41 -0500

    [UPDATE](install_bash)Add (nerd-fonts)
    Include cloning (nerd-fonts) repository in installation process.

--- bash/install_bash.sh
+++ bash/install_bash.sh
@@ -25,6 +25,7 @@ __configBASH(){
         dLog "${BLUE}Installing Base16-Shell..."
         /usr/bin/env git clone "https://github.com/chriskempson/base16-shell.git" "$BASE16SHELL"
         dLog "${BLUE}Installing Base16-Shell...DONE"
+        /usr/bin/env git clone "https://github.com/ryanoasis/nerd-fonts.git"
     else
         dLog "${BLUE}Updating Base16-Shell..."
         echo "${BASE16SHELL}"
@@ -32,6 +33,19 @@ __configBASH(){
         /usr/bin/env git pull origin
         cd "$_PWD" || error "32:install_bash.sh" "Unable to CD into ${_PWD}"
         dLog "${BLUE}Updating Base16-Shell...DONE"
+    fi
+    local NERDFONTS="${_BIN}/nerd-fonts"
+    if [[ ! -d $NERDFONTS ]]; then
+        dLog "${BLUE}Installing Nerd-Fonts..."
+        /usr/bin/env git clone "https://github.com/ryanoasis/nerd-fonts.git" "$NERDFONTS"
+        dLog "${BLUE}Installing Nerd-Fonts...DONE"
+    else
+        dLog "${BLUE}Updating Nerd-Fonts..."
+        echo "${NERDFONTS}"
+        cd "${NERDFONTS}" || error "45:install_bash.sh" "Unable to CD into ${NERDFONTS}"
+        /usr/bin/env git pull origin
+        cd "$_PWD" || error "47:install_bash.sh" "Unable to CD into ${_PWD}"
+        dLog "${BLUE}Updating Nerd-Fonts...DONE"
     fi
 
     dLog "${BLUE}Bash Shell Configuration...DONE"
